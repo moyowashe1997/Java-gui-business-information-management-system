052a95f9fa808b183b5d01ae6d40b6ac7e6a7b0e
Moyowashe97 <excellentgarawaziva@gmail.com>
2020-03-25 21:22:54 +0200

    First commit

---
+++ src/PRG_Assignment/Assignment.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PRG_Assignment;
+import java.util.Scanner;
+/**
+ *
+ * @author Ruaan Cloete
+ */
+public class Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner r = new Scanner(System.in);
+        new LandingPage().setVisible(true);
+        
+    }
+ 
+}
